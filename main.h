78b82595401875d73774e1aad1913d94fe083ad1
Emmanuel Ademola <mailemmydee@gmail.com>
2023-08-22 23:13:12 -0700

    Header updated

--- main.h
+++ main.h
@@ -35,6 +35,21 @@ typedef struct data
 	int status;
 } shell_state;
 
+/**
+ * struct str_var_list - linked list for str_var
+ * @valLen: length of the value
+ * @varLen: length of the variable
+ * @varVal: value of the variable
+ * @next: next node in list
+ */
+typedef struct str_var_list
+{
+	int valLen;
+	int varLen;
+	char *varVal;
+	struct str_var_list *next;
+} s_var;
+
 int main(int ac, char **av, char **env);
 void handle_sigint(int dummy);
 void data_init(shell_state *shelldata, char **argv);
@@ -49,5 +64,15 @@ void theshell(shell_state *shelldata);
 char *remove_comment(char *in);
 int first_non_whitespace(char *input, int *index);
 int check_error(shell_state *shelldata, char *input);
+void print_error(shell_state *shelldata, char *input, int i, int bool);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, const char *src);
+void print_error(shell_state *shelldata, char *input, int i, int bool);
+char *construct_err(shell_state *shdata, char *i, char *m1, char *m2, char *m3);
+void print_error_message(char *error_message);
+char *determine_error_message(char *input, int i, int bool);
+int error_sep(char *input, int i, char last);
+int xRepeatedChar(char *input, int i);
+char *str_var(char *input, shell_state *shelldata);
 
 #endif
